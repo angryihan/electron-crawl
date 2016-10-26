b5fe80b2a5e6a79c598c42e2d4780741b0aa498e
yihan <495552532@qq.com>
2016-10-26 18:36:28 +0800

    fix

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,21 +1,21 @@
-var clipboard = require('electron').clipboard;
-var crawler = require('./crawler.js');
+const clipboard = require('electron').clipboard;
+const crawler = require('./crawler.js');
 
-var generateBtn = document.querySelector("#generate");
-var copyBtn = document.querySelector("#copy");
-var urlInput = document.querySelector("#url");
-var keywordInput = document.querySelector("#keyword");
-var result = document.querySelector("#result");
-var inputList = document.querySelectorAll(".JS-input");
-// var channelCheckBox = document.querySelector("#isChannel");
+const generateBtn = document.querySelector("#generate");
+const copyBtn = document.querySelector("#copy");
+const urlInput = document.querySelector("#url");
+const keywordInput = document.querySelector("#keyword");
+const result = document.querySelector("#result");
+const inputList = document.querySelectorAll(".JS-input");
+// const channelCheckBox = document.querySelector("#isChannel");
 
-var lvmamaCrawler = {
+const lvmamaCrawler = {
     init: function() {
         result.value = '提示：\n1.网址输入框支持多网址，以逗号分隔\n2.网址输入框为空表示搜索全站频道页';
         this.bindEvents();
     },
     bindEvents: function() {
-        var self = this;
+        let self = this;
         generateBtn.addEventListener("click", self.generateHandler);
         copyBtn.addEventListener("click", self.copyHandler);
         inputList.forEach(function(input) {
@@ -28,8 +28,8 @@ var lvmamaCrawler = {
     },
     generateHandler: function() {
         generateBtn.blur();
-        var urlString = urlInput.value;
-        var keyword = keywordInput.value;
+        let urlString = urlInput.value;
+        let keyword = keywordInput.value;
         // var isChannel = channelCheckBox.checked;
         if (!keyword) {
             result.value = '请输入关键词';
