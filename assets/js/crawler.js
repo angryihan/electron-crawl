014ed04b618a8094c44f9b0ee06849c255becdfe
angryihan <495552532@qq.com>
2016-12-30 09:56:51 +0800

    Revert "fix"
    This reverts commit b5fe80b2a5e6a79c598c42e2d4780741b0aa498e.

--- assets/js/crawler.js
+++ assets/js/crawler.js
@@ -1,19 +1,18 @@
-const async = require('async');
-const request = require('request');
-const cheerio = require('cheerio');
-const channelUrl = require('./channelUrl.js');
-let option = {
+var async = require('async');
+var request = require('request');
+var cheerio = require('cheerio');
+var channelUrl = require('./channelUrl.js');
+var option = {
     headers: {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
     }
 }
-let keyword = '';
-let linkArray = [];
 
 module.exports = {
-    getLinks: function(urlString, keywordString, cb) {
-        keyword = keywordString;
-        let urlArray = urlString ? urlString.split(',') : channelUrl.channelUrlArray;
+    getLinks: function(urlString, keyword, cb) {
+        var linkArray = [];
+        var pattern = new RegExp("(pic\.lvmama\.com.*" + keyword + ")|(s\\d\.lvjs\.com\.cn.*" + keyword + ")");
+        var urlArray = urlString ? urlString.split(',') : channelUrl.channelUrlArray;
         async.eachSeries(urlArray, crawlLinks, function(err) {
             if (err) {
                 console.log(err);
@@ -21,39 +20,38 @@ module.exports = {
                 cb(linkArray.length ? toText(linkArray) : '未找到对应信息');
             }
         });
+
+        function crawlLinks(url, callback) {
+            option.url = url;
+            request(option, function(error, res, body) {
+                if (!error && res.statusCode == 200) {
+                    var $ = cheerio.load(body);
+                    var $resources = $('link[rel="stylesheet"], script');
+                    $resources.each(function(index, ele) {
+                        var link = $(ele).attr('href') ? $(ele).attr('href') : $(ele).attr('src');
+                        if (pattern.test(link)) {
+                            linkArray.push(link);
+                        }
+                    });
+                    callback(null);
+                } else {
+                    callback(null);
+                }
+            });
+        }
     }
 }
 
-function crawlLinks(url, callback) {
-    option.url = url;
-    let pattern = new RegExp("(pic\.lvmama\.com.*" + keyword + ")|(s\\d\.lvjs\.com\.cn.*" + keyword + ")");
-    request(option, function(error, res, body) {
-        if (!error && res.statusCode == 200) {
-            let $ = cheerio.load(body);
-            let $resources = $('link[rel="stylesheet"], script');
-            $resources.each(function(index, ele) {
-                let link = $(ele).attr('href') ? $(ele).attr('href') : $(ele).attr('src');
-                if (pattern.test(link)) {
-                    linkArray.push(link);
-                }
-            });
-            callback(null);
-        } else {
-            callback(null);
-        }
-    });
-}
-
 function toText(arr) {
-    let text = '';
-    for (let i = 0; i < arr.length; i++) {
+    var text = '';
+    for (var i = 0; i < arr.length; i++) {
         text += generateAllLinks(arr[i]);
     }
     return text;
 }
 
 function generateAllLinks(url) {
-    let allLinks = url.replace(/(pic\.lvmama\.com)|(s\d\.lvjs\.com\.cn)/, "pic.lvmama.com");
+    var allLinks = url.replace(/(pic\.lvmama\.com)|(s\d\.lvjs\.com\.cn)/, "pic.lvmama.com");
     allLinks = allLinks + '\n' + allLinks.replace(/pic.lvmama.com/, "s1.lvjs.com.cn") + '\n' + allLinks.replace(/pic.lvmama.com/, "s2.lvjs.com.cn") + '\n' + allLinks.replace(/pic.lvmama.com/, "s3.lvjs.com.cn") + '\n';
     return allLinks + '\n';
 }
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,21 +1,21 @@
-const clipboard = require('electron').clipboard;
-const crawler = require('./crawler.js');
+var clipboard = require('electron').clipboard;
+var crawler = require('./crawler.js');
 
-const generateBtn = document.querySelector("#generate");
-const copyBtn = document.querySelector("#copy");
-const urlInput = document.querySelector("#url");
-const keywordInput = document.querySelector("#keyword");
-const result = document.querySelector("#result");
-const inputList = document.querySelectorAll(".JS-input");
-// const channelCheckBox = document.querySelector("#isChannel");
+var generateBtn = document.querySelector("#generate");
+var copyBtn = document.querySelector("#copy");
+var urlInput = document.querySelector("#url");
+var keywordInput = document.querySelector("#keyword");
+var result = document.querySelector("#result");
+var inputList = document.querySelectorAll(".JS-input");
+// var channelCheckBox = document.querySelector("#isChannel");
 
-const lvmamaCrawler = {
+var lvmamaCrawler = {
     init: function() {
         result.value = '提示：\n1.网址输入框支持多网址，以逗号分隔\n2.网址输入框为空表示搜索全站频道页';
         this.bindEvents();
     },
     bindEvents: function() {
-        let self = this;
+        var self = this;
         generateBtn.addEventListener("click", self.generateHandler);
         copyBtn.addEventListener("click", self.copyHandler);
         inputList.forEach(function(input) {
@@ -28,8 +28,8 @@ const lvmamaCrawler = {
     },
     generateHandler: function() {
         generateBtn.blur();
-        let urlString = urlInput.value;
-        let keyword = keywordInput.value;
+        var urlString = urlInput.value;
+        var keyword = keywordInput.value;
         // var isChannel = channelCheckBox.checked;
         if (!keyword) {
             result.value = '请输入关键词';
